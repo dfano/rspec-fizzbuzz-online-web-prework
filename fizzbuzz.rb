be37140b9e6b2c2200a353dd03d53060fc518c40
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-08 19:19:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-100.times do |f|
-  f = "test"
-  puts f
-end
+1 % 3
+2 % 3
+3 % 3
