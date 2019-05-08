d83a8e7d0ff3c56d0d09ac11aca1bc704727f66b
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-08 19:21:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-puts 1 % 3
-puts 2 % 3
-puts 3 % 3
+num = 3
 
-if 3 % 3 == 0
+if num % 3 == 0
   puts "fizz"
 else
   puts "no"
